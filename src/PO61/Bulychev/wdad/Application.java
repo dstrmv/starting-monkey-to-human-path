e7902d89cf6aff71626ab04030425d937ae081ab
dstrmv <dstrmv@outlook.com>
2018-09-20 08:53:10 +0400

    added entry point

---
+++ src/PO61/Bulychev/wdad/Application.java
@@ -0,0 +1,7 @@
+package PO61.Bulychev.wdad;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("I'm Ivan Bulychev, and I'm not a monkey");
+    }
+}
