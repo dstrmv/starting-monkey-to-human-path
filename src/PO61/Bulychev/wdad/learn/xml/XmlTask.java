7fe8c1f11a1d1e8bb2bb4c964629d604ad338cb9
dstrmv <dstrmv@outlook.com>
2018-09-20 10:09:26 +0400

    added first xml

--- src/PO61/Bulychev/wdad/learn/xml/XmlTask.java
+++ src/PO61/Bulychev/wdad/learn/xml/XmlTask.java
@@ -1,4 +1,20 @@
 package PO61.Bulychev.wdad.learn.xml;
 
+import java.util.Calendar;
+
 public class XmlTask {
+
+    public int earningsTotal(String officiantSecondName, Calendar calendar) {
+        return 0;
+    }
+
+    public void removeDay(Calendar calendar) {
+
+    }
+
+    public void changeOfficiantName(String oldFirstName, String oldSecondName,
+                                    String newFirstName, String newSecondName) {
+
+    }
+
 }
