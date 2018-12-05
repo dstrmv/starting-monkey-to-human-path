161d09adddbeffeeaa741c3f7585c4c04686e2b3
dstrmv <dstrmv@outlook.com>
2018-12-05 19:53:31 +0400

    lb3 started

--- src/PO61/Bulychev/wdad/data/managers/PreferencesManager.java
+++ src/PO61/Bulychev/wdad/data/managers/PreferencesManager.java
@@ -24,72 +24,24 @@ public class PreferencesManager {
     private String xmlpath = "src/PO61/Bulychev/wdad/resources/configuration/appconfig.xml";
     private XPathFactory xPathFactory;
     private Document document;
-    private Map<String, String> propertyToXPath;
+    //private Map<String, String> propertyToXPath;
 
     private PreferencesManager() {
         loadxml(xmlpath);
         xPathFactory = XPathFactory.newInstance();
-        propertyToXPath = new HashMap<>();
-        propertyToXPath.put("createregistry", "/appconfig/rmi/server/registry/createregistry/text()");
-        propertyToXPath.put("registryaddress", "/appconfig/rmi/server/registry/registryaddress/text()");
-        propertyToXPath.put("registryport", "/appconfig/rmi/server/registry/registryport/text()");
-        propertyToXPath.put("policypath", "/appconfig/rmi/client/policypath/text()");
-        propertyToXPath.put("usecodebaseonly", "/appconfig/rmi/client/usecodebaseonly/text()");
-        propertyToXPath.put("classprovider", "/appconfig/rmi/classprovider/text()");
+//        propertyToXPath = new HashMap<>();
+//        propertyToXPath.put("createregistry", "/appconfig/rmi/server/registry/createregistry/text()");
+//        propertyToXPath.put("registryaddress", "/appconfig/rmi/server/registry/registryaddress/text()");
+//        propertyToXPath.put("registryport", "/appconfig/rmi/server/registry/registryport/text()");
+//        propertyToXPath.put("policypath", "/appconfig/rmi/client/policypath/text()");
+//        propertyToXPath.put("usecodebaseonly", "/appconfig/rmi/client/usecodebaseonly/text()");
+//        propertyToXPath.put("classprovider", "/appconfig/rmi/classprovider/text()");
     }
 
     public static PreferencesManager getInstance() {
         return ourInstance;
     }
 
-    public String getCreateRegistryProperty() {
-        return getProperty("createregistry");
-    }
-
-    public void setCreateRegistryProperty(String val) {
-        setProperty("createregistry", val);
-    }
-
-    public String getRegistryAddressProperty() {
-        return getProperty("registryaddress");
-    }
-
-    public void setRegistryAddressProperty(String val) {
-        setProperty("registryaddress", val);
-    }
-
-    public String getRegistryPortProperty() {
-        return getProperty("registryport");
-    }
-
-    public void setRegistryPortProperty(String val) {
-        setProperty("registryport", val);
-    }
-
-    public String getPolicyPathProperty() {
-        return getProperty("policypath");
-    }
-
-    public void setPolicyPathProperty(String val) {
-        setProperty("policypath", val);
-    }
-
-    public String getUseCodeBaseOnlyProperty() {
-        return getProperty("usecodebaseonly");
-    }
-
-    public void setUseCodeBaseOnlyProperty(String val) {
-        setProperty("usecodebaseonly", val);
-
-    }
-
-    public String getClassProviderProperty() {
-        return getProperty("classprovider");
-    }
-
-    public void setClassProviderProperty(String val) {
-        setProperty("classprovider", val);
-    }
 
     public void setProperty(String property, String value) {
         getNode(property).setTextContent(value);
@@ -98,12 +50,13 @@ public class PreferencesManager {
 
     public String getProperty(String property) {
 
-        String xpathProperty = propertyToXPath.get(property);
+        //String xpathProperty = propertyToXPath.get(property);
         XPath xpath = xPathFactory.newXPath();
         String result = "";
         XPathExpression expr = null;
         try {
-            expr = xpath.compile(xpathProperty);
+            //expr = xpath.compile(xpathProperty);
+            expr = xpath.compile(property);
             result = (String) expr.evaluate(document, XPathConstants.STRING);
         } catch (XPathExpressionException e) {
             e.printStackTrace();
@@ -141,4 +94,65 @@ public class PreferencesManager {
         return appconfig.item(0);
     }
 
+
+    @Deprecated
+    public String getCreateRegistryProperty() {
+        return getProperty("createregistry");
+    }
+
+    @Deprecated
+    public void setCreateRegistryProperty(String val) {
+        setProperty("createregistry", val);
+    }
+
+    @Deprecated
+    public String getRegistryAddressProperty() {
+        return getProperty("registryaddress");
+    }
+
+    @Deprecated
+    public void setRegistryAddressProperty(String val) {
+        setProperty("registryaddress", val);
+    }
+
+    @Deprecated
+    public String getRegistryPortProperty() {
+        return getProperty("registryport");
+    }
+
+    @Deprecated
+    public void setRegistryPortProperty(String val) {
+        setProperty("registryport", val);
+    }
+
+    @Deprecated
+    public String getPolicyPathProperty() {
+        return getProperty("policypath");
+    }
+
+    @Deprecated
+    public void setPolicyPathProperty(String val) {
+        setProperty("policypath", val);
+    }
+
+    @Deprecated
+    public String getUseCodeBaseOnlyProperty() {
+        return getProperty("usecodebaseonly");
+    }
+
+    @Deprecated
+    public void setUseCodeBaseOnlyProperty(String val) {
+        setProperty("usecodebaseonly", val);
+
+    }
+
+    @Deprecated
+    public String getClassProviderProperty() {
+        return getProperty("classprovider");
+    }
+
+    @Deprecated
+    public void setClassProviderProperty(String val) {
+        setProperty("classprovider", val);
+    }
 }
